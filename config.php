c9347d7bfb5fc1e5db188071bc32b3bc44f44262
Katie Stadler <katiehstadler@gmail.com>
2021-06-08 17:09:39 -0400

    dsn

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $host       = "localhost";
 $username   = "root"; // admin 
 $password   = "root"; // password 
 $dbname     = "test"; // dbproj 
-$dsn        = "mysql:host=$host;dbname=$dbname";
+$dsn        = "mysql:host=$host;dbname=$dbname"; // mysql:host=localhost;dbname=dbproj
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
