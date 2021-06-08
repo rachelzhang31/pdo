e8654c9754677410ff7af5f3a7cb769b413a10b4
Katie Stadler <katiehstadler@gmail.com>
2021-06-08 16:53:42 -0400

    comments

--- config.php
+++ config.php
@@ -6,9 +6,9 @@
  */
 
 $host       = "localhost";
-$username   = "root";
-$password   = "root";
-$dbname     = "test";
+$username   = "root"; // admin 
+$password   = "root"; // password 
+$dbname     = "test"; // dbproj 
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
