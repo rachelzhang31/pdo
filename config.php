7273a241d62164f566a5a517cfb0c3c3c01560f1
Katie Stadler <katiehstadler@gmail.com>
2021-06-08 20:26:49 -0400

    update configuration 2

--- config.php
+++ config.php
@@ -7,9 +7,34 @@
 
 $host       = "localhost";
 $username   = "root"; // admin 
-$password   = "root"; // password 
-$dbname     = "test"; // dbproj 
-$dsn        = "mysql:host=$host;dbname=$dbname"; // mysql:host=localhost;dbname=dbproj
+$password   = ""; // password 
+$dbname     = "dbproj"; // dbproj 
+// dsn below
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-              );
+              ); 
+
+/* 
+LINK TO PHP MY ADMIN 
+*/ 
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+
+try {
+   $db = new PDO($dsn, $username, $password); // try to connect to the database 
+} 
+catch (PDOException $e) { // handle a PDO exception 
+   $error_message = $e->getMessage();
+   echo "<p>An error occurred while connecting to the database: $error_message </p>";
+} 
+catch (Exception $e) { // handle any type of exception
+   $error_message = $e->getMessage();
+   echo "<p>Error message: $error_message </p>";
+}
+
+?>
+
+<?php
+// to close a connection 
+// $db = null;
+?>
