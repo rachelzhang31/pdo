702bba5de5d3f848ad124650e0b34f0f29b17b15
Katie Stadler <katiehstadler@gmail.com>
2021-06-10 01:02:46 -0400

    style changes

--- public/index.php
+++ public/index.php
@@ -1,10 +1,16 @@
 <?php include "templates/header.php"; ?>
 
 <ul>
-	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
-	<li><a href="update.php"><strong>Update</strong></a> - edit a user</li>
-	<li><a href="delete.php"><strong>Delete</strong></a> - delete a user</li>
+	<div class="grid-container"> 
+		<div class="item1" style="display: flex; justify-content: flex-end;"> <button class="land" onclick="location.href='create.php'"> Register a Madison House Member </button> 
+		</div> 
+		<div class="item2"> <button class="land" onclick="location.href='read.php'"> Find a Member of Madison House </button>
+		</div>
+		<div class="item3" style="display: flex; justify-content: flex-end;"> <button class="land" onclick="location.href='update.php'"> Update a Madison House Member </button>
+		</div>
+		<div class="item4"> <button class="land" onclick="location.href='delete.php'"> Delete a Madison House Member </button>
+		</div>
+	</div> 
 </ul>
 
 <?php include "templates/footer.php"; ?>
